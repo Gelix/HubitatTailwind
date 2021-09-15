a3a158ca12bb17775189ee86a2c2ab340e23d197
Gelix <derek.badge@gmail.com>
2021-09-15 10:22:16 -0400

    Bug Squashing!
    -fixed the polling after open/close  Should now detect close/open as fast or faster than the Tailwind App.
    -Added configurable timeout to wait for doors to open/close
    -default polling interval should be in minutes, this catches someone who uses a remote or the app to open/close the doors only.

--- tailwinddriver.groovy
+++ tailwinddriver.groovy
@@ -3,7 +3,8 @@ preferences {
     input name: "cName", type: "string", title: "Tailwind Controller Name", required: "True",  description: '<em>Changes the name for the controller displayed in dashboards, DOES affect children unique deviceNetworkId.  Changing this will re-create the children devices.</em>'
     //input name: "token", type: "password", title: "Access Token", required: "True"
     input name: "doorCount", type: "number", title: "Number of Doors", required: "True", range: "0..3", defaultValue : 1
-    input name: "interval", type: "number", title: "Polling interval (seconds)", required: "True", range: "1..59", defaultValue : 30
+    input name: "interval", type: "enum", title: "Polling interval", required: "True", options: ["1", "5", "10", "15", "30"], defaultValue : 1, description: '<em> Minutes, if you want it more frequently, use Rules.</em>'
+    input name: "garageDoorTimeout", type: "number", title: "Door Open/Close timeout", required: "True", defaultValue : 60, description: '<em> Seconds. How long should faster polling be run before giving up on waiting to check and see if the door status has changed after issuing a command.</em>'
     input name: "debugEnable", type: "bool", title: "Enable debug logging?", defaultValue: true,  description: '<em>for 2 hours</em>'
     if(doorCount > 0){input name: "d1Name", type: "string", title: "Door 1 Name", required: "false", defaultValue : "Door 1",  description: '<em>Changes the name for Door 1 displayed in dashboards, does not affect children unique deviceNetworkId.  Changing this will have no effect on the children devices being re-created.</em>'}
     if(doorCount > 1){input name: "d2Name", type: "string", title: "Door 2 Name", required: "false", defaultValue : "Door 2",  description: '<em>Changes the name for Door 1 displayed in dashboards, does not affect children unique deviceNetworkId.  Changing this will have no effect on the children devices being re-created.</em>'}
@@ -44,10 +45,15 @@ def updated() {
 }
 
 def init() {
-    log.info "Scheduling Polling interval for ${settings.interval} second(s)..."    
+    log.info "Scheduling Polling interval for ${settings.interval} minute(s)..."    
     addChildren()
     sendEvent(name: "Status", value: 0)
-    schedule("0/${settings.interval} * * ? * * *", poll)
+    //schedule("0/${settings.interval} * * ? * * *", poll)
+    if (settings.interval == "1") runEvery1Minute(poll)
+    else if (settings.interval == "5") runEvery5Minutes(poll)
+    else if (settings.interval == "10") runEvery10Minutes(poll)
+    else if (settings.interval == "15") runEvery15Minutes(poll)
+    else if (settings.interval == "30") runEvery30Minutes(poll)
     poll()    
 }
 
@@ -84,17 +90,15 @@ void addChildren(){
                 log.error "Could not create child device"
             }
         }
-        if(debugEnable) log.debug "deviceNetworkId ${cd.deviceNetworkId}=${cName}:${d}"        
-        if(debugEnable) log.debug "name ${cd.name}=${d}"
-        if(debugEnable) log.debug "label ${cd.label}=${cName} : ${dn}"
+        if(debugEnable) log.debug "deviceNetworkId ${cd.deviceNetworkId}=${cName}:${d} name ${cd.name}=${d} label ${cd.label}=${cName} : ${dn}"        
         if(cd.label != "${cName} : ${dn}")
         {
-            log.debug "label mismatch ${cd.label}=${cName} : ${dn}"
+            log.debug "Correcting child label mismatch ${cd.label}=${cName} : ${dn}"
             cd.label = "${cName} : ${dn}"
         }
         if(cd.name != "${d}")
         {
-            log.debug "name mismatch ${cd.name}=${d}"
+            log.debug "Correcting child name mismatch ${cd.name}=${d}"
             cd.name = "${d}"
         }
     }
@@ -104,7 +108,7 @@ void addChildren(){
 def poll() {
     def s = checkStatus()
     def old = device.currentValue("Status").toInteger()
-    //only set status if it changed.
+    //only set status if it changed. a lot less event spam this way.
     if(s != old)
     {
         log.debug "Status changed from ${old} to ${s}"
@@ -115,55 +119,68 @@ def poll() {
 def openClose(String command, Integer doorNumber){
     def desiredStatus = "closed"
     def Integer cmd = doorNumber * -1
+    if(doorNumber == 3) cmd = cmd - 1 //assuming documentation is correct on -4 and 4 for door #3, another user reported this being 3, but mine still seems to be 4.
     if(command == "open")
     {
         desiredStatus = "open"
-        cmd = doorNumber
+        cmd = cmd * -1
     }
     if(debugEnable) log.debug "Attempting to ${command} door ${doorNumber}"
     def postParams = [uri: "http://${IP}/cmd", body : "${cmd}"]     
-    log.debug postParams
+    if(debugEnable) log.debug postParams
     httpPost(postParams) { def resp ->
-        if(debugEnable) log.debug "${command} Response: ${resp.data}"     
+        if(debugEnable) log.debug "${command} Response (should match ${cmd}): ${resp.data}"     
         if ("${resp.data}" == "${cmd}" )
         {
+            log.debug "in 1 second, start polling every 5 seconds for door to open/close."
             //schedule to run the refresh for rapid updates on dashboard
             runIn(1, "postActionRefresh", [data:["desiredStatus":"${desiredStatus}","doorNumber":doorNumber]])
         }
     }
 }
 
-void postActionRefresh(data){    
-    String desiredStatus=data.get("desiredStatus")
-    Integer doorNumber = data.get("doorNumber")
-    if(debugEnable) log.debug "Door #${doorNumber} Desired Status: ${desiredStatus} Current status: ${doorCheck(doorNumber)}"
+void postActionRefresh(data){ 
+    log.debug "Now polling every 2 seconds for door to open/close."
+    String desiredStatus = data.get("desiredStatus")
+    def Integer doorStatus = checkStatus()
+    Integer doorNumber = data.get("doorNumber").toInteger()
+    if(debugEnable) log.debug "${doorStatus} Door #${doorNumber} Desired Status: ${desiredStatus} Current status: ${doorCheck(doorNumber,doorStatus)}"
     def Integer i = 0 //count seconds elapsed after door command
-    while ( doorCheck(doorNumber) != desiredStatus){                
-        checkStatus()
-        if(debugEnable) log.debug "Door #${doorNumber} Desired Status: ${desiredStatus} Current status: ${doorCheck(doorNumber)}"
-        pauseExecution(5000)
+    
+    while ( doorCheck(doorNumber, doorStatus) != desiredStatus){                
+        doorStatus = checkStatus()
+        if(debugEnable) log.debug "Door #${doorNumber} Desired Status: ${desiredStatus} Current status: ${doorCheck(doorNumber,doorStatus)}"
+        pauseExecution(2000)
         //break out of loop after a period, infinite loops are bad
-        i += 1
-        if(i >= 60)
+        i += 2
+        if(i >= garageDoorTimeout)
         {
-            if(debugEnable) log.debug "60 seconds is too long for a door, probably something went wrong physically (blocked sensor, stuck/etc)."
+            if(debugEnable) log.debug "${garageDoorTimeout} seconds is too long for a door, probably something went wrong physically (blocked sensor, stuck/etc)."
             break
         }
     }
-    if(debugEnable) log.debug "Door #${doorNumber} Desired Status: ${desiredStatus} Current status: ${doorCheck(doorNumber)}"          
+    if (doorCheck(doorNumber, doorStatus) == desiredStatus){setDoorStatus(doorStatus)}
+    
+    if(debugEnable) log.debug "Door #${doorNumber} Desired Status: ${desiredStatus} Current status: ${doorCheck(doorNumber,doorStatus)}"          
 }
 
-def doorCheck(Integer doorNumber){
-    retval =getDoorOpenClose(getDoorStatus(device.currentValue("Status").toInteger(),doorNumber))
-    if (debugEnabled) log.debug "Door ${doorNumber} is ${retval}"
-    return retval
+def doorCheck(Integer doorNumber, Integer doorStatus){
+    checkNumber = doorNumber -1 //the statusCode is 0,1,2 so subtract 1 
+    r =getDoorOpenClose(getDoorStatus(doorStatus,checkNumber))
+    if(debugEnable) log.debug "trying to get open/close from ${doorStatus} ${checkNumber}"
+    if(debugEnable) log.debug "getDoorStatus returned ${getDoorStatus(doorStatus,checkNumber)}"
+    if(debugEnable) log.debug "Door ${doorNumber} is ${r}"
+    return r
 }
+
 def childOpen(Integer doorNumber){
     open(doorNumber)
 }
+
 def childClose(Integer doorNumber){
     close(doorNumber)
 }
+
 def open(Integer doorNumber) {
     openClose("open",doorNumber)
 }
@@ -201,15 +218,7 @@ def getDoorStatus(Integer status, Integer door){
           [-1, -2, 4],    
           [1, -2, 4],     
           [-1, 2, 4],     
-          [1, 2, 4],
-          [-1, -2, -3],   
-          [1, -2, -3],    
-          [-1, 2, -3],    
-          [1, 2, -3],     
-          [-1, -2, 3],    
-          [1, -2, 3],     
-          [-1, 2, 3],     
-          [1, 2, 3]
+          [1, 2, 4]
         ] 
     return statusCodes[status][door]
 }
